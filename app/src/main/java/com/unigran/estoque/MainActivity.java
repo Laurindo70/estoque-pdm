7205504ab62410f7fab3e45aa96d1038d75f79df
Ronan Laurindo Flor <ronanlf12@gmail.com>
2022-10-18 17:14:02 -0400

    Primeira tela finalizada

--- app/src/main/java/com/unigran/estoque/MainActivity.java
+++ app/src/main/java/com/unigran/estoque/MainActivity.java
@@ -3,12 +3,18 @@ package com.unigran.estoque;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button sair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sair = findViewById(R.id.btn_sair);
+        //sair.setBackgroundResource(R.color.black);
     }
 }
